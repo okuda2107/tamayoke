54d3f3be9f0680b408b46b0d79c1f831732c30d9
okuda <okuda2107@gmail.com>
2023-10-17 23:19:52 +0900

    omake

---
+++ LevelLoader.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import json
+from game import *
+from actor import *
+
+def loadLevel(game: Game, file_name: str):
+    pass
+
+class LevelLoader:
+    def __init__(self):
+        pass
