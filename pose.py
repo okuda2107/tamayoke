b1d6210cca2964a53f726d8f4fcdefe3adcf113f
okuda <okuda2107@gmail.com>
2024-08-30 23:54:15 +0900

    poseフレームが描画されるようにした

--- pose.py
+++ pose.py
@@ -1,21 +1,60 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-
+import pygame
 from sprite_component import SpriteComponent
 
 if TYPE_CHECKING:
     from pygame import Surface
     from actor import Actor
 
+
 class Pose(SpriteComponent):
-    def __init__(self, owner: Actor, draw_order: int = 100):
+    def __init__(self, owner: Actor, draw_order: int = 10):
         super().__init__(owner, draw_order)
-        self.color = (0, 0, 0)
+        self.color = (255, 255, 255)
+        self.face_radius = 200
         self.radius = 20
         self.thickness = 8
 
     def __del__(self):
         return super().__del__()
-    
+
     def draw(self, screen: Surface) -> None:
-        pass
+        pose = self._owner.game.mediapipe.result.pose_landmarks.landmark
+        screen_size = self._owner.game.screen_size
+
+        # 顔描画
+        pygame.draw.circle(
+            screen,
+            self.color,
+            (pose[0].x * screen_size[0], pose[0].y * screen_size[1]),
+            self.face_radius,
+            self.thickness,
+        )
+
+        # 関節描画
+        for i in [11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]:
+            pygame.draw.circle(
+                screen,
+                self.color,
+                (pose[i].x * screen_size[0], pose[i].y * screen_size[1]),
+                self.radius,
+            )
+
+        # 関節間の描画
+        line_start = [11, 11, 11, 12, 12, 13, 14, 23, 23, 24, 25, 26]
+        line_end = [12, 13, 23, 14, 24, 15, 16, 24, 25, 26, 27, 28]
+        for (start, end) in zip(line_start, line_end):
+            pygame.draw.line(
+                screen,
+                self.color,
+                (
+                    pose[start].x * screen_size[0],
+                    pose[start].y * screen_size[1],
+                ),
+                (
+                    pose[end].x * screen_size[0],
+                    pose[end].y * screen_size[1],
+                ),
+                self.thickness,
+            )
--- test.py
+++ test.py
@@ -10,11 +10,13 @@ import pygame
 import numpy as np
 from mediapipe_input import MediapipeInput
 from pose import Pose
+from pointer import Pointer
 
 class Test(Actor):
     def __init__(self, game: Game):
         super().__init__(game)
-        # sc = SpriteComponent(self)
+        # actor = Pointer(self.game, 15)
+        # sc = SpriteComponent(actor)
         # sc.set_image('asset/pointer.png', (20, 20))
         p = Pose(self)
 
@@ -23,13 +25,3 @@ class Test(Actor):
 
     def load_properties(self, obj: dict[str, Any]) -> None:
         return super().load_properties(obj)
-
-    def actor_input(self, event) -> None:
-        super().actor_input(event)
-        pose = self.game.mediapipe.detect_pose()
-        if pose != None:
-            self.position = np.array([pose.pose_landmarks.landmark[15].x, pose.pose_landmarks.landmark[15].y])
-        
-    def update_actor(self, delta_time: float) -> None:
-        super().update_actor(delta_time)
-
