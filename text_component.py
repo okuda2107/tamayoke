ee59786f650fc0ba1d4943491c08a4e1bc87f0c6
okuda <okuda2107@gmail.com>
2024-09-02 10:32:22 +0900

    コード整理

--- text_component.py
+++ text_component.py
@@ -5,13 +5,17 @@ from sprite_component import *
 
 # シンプルにmicrosoftsansserifでいいかな
 class TextComponent(SpriteComponent):
-    def __init__(self, owner: Actor, font_name: str, size: int):
+    def __init__(self, owner: Actor, size: int):
         super().__init__(owner)
-        self.font = pygame.font.SysFont(font_name, size)
+        self.font = pygame.font.SysFont('microsoftsansserif', size)
+        self.size = size
         self.text: pygame.Surface
 
     def set_text(self, text: str, color: tuple[int, int,int]):
         self.text = self.font.render(text, True, color)
+
+    def set_font(self, font: str):
+        self.font = pygame.font.SysFont(font, self.size)
 
     def draw(self, screen: pygame.Surface) -> None:
         position = self._calc_disp_position()
