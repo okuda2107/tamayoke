7f531445bbece94686b11957a3c4341bdd5c5942
okuda <okuda2107@gmail.com>
2024-09-10 23:01:18 +0900

    move_componentにまとめた

--- core.py
+++ core.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 import numpy as np
-import copy
 import random
 from actor import Actor
 from collision import Sphere
 from sprite_component import SpriteComponent
+from move_component import MoveComponent
 from circle_component import CircleComponent, Kind
 
 if TYPE_CHECKING:
@@ -15,8 +15,9 @@ class Core(Actor):
     def __init__(self, game: Game):
         super().__init__(game)
         rng = np.random.default_rng()
-        self.forward: np.Array = rng.uniform(-1, 1, 2)
-        self.speed = random.uniform(0, 1)
+        mc = MoveComponent(self)
+        mc.set_forward(rng.uniform(-1, 1, 2))
+        mc.speed = 0.1
         sc = SpriteComponent(self)
         sc.set_image('asset/red.png', (200, 200))
         cc = CircleComponent(self, Kind.core)
@@ -28,13 +29,3 @@ class Core(Actor):
     def __del__(self):
         return super().__del__()
     
-    def update_actor(self, delta_time: float) -> None:
-        super().update_actor(delta_time)
-        pre_position = copy.deepcopy(self.position)
-        self.position = self.position + self.forward * self.speed * delta_time
-        if ((self.position <= np.zeros(2)) | (np.ones(2) <= self.position)).any() and ((np.zeros(2) < pre_position) & (pre_position < np.ones(2))).any():
-            if self.position[0] <= 0 or self.position[0] >= 1:
-                self.forward[0] *= -1
-            if self.position[1] <= 0 or self.position[1] >= 1:
-                self.forward[1] *= -1
-        
--- move_component.py
+++ move_component.py
@@ -1,15 +1,32 @@
 from __future__ import annotations
-from game import *
-#作りかけ
-from actor import *
-from component import *
+from typing import TYPE_CHECKING
+import copy
+import numpy as np
+from actor import Actor
+from component import Component
+
+if TYPE_CHECKING:
+    from game import Game
 
 class MoveComponent(Component):
     def __init__(self, owner: Actor):
         super().__init__(owner)
+        self.speed: float = 0.0
+        self.forward: np.ndarray = np.array([0.0, 0.0])
     
     def __del__(self):
         super().__del__()
 
+    # 正規化してくれる
+    def set_forward(self, direction: np.ndarray):
+        self.forward = direction / np.linalg.norm(direction, ord=2)
+
+# 反射も含めた
     def update(self, delta_time: float):
-        self._owner.position = self._owner.position + self._owner.velocity * delta_time
+        pre_position = copy.deepcopy(self._owner.position)
+        self._owner.position = self._owner.position + self.forward * self.speed * delta_time
+        if ((self._owner.position <= np.zeros(2)) | (np.ones(2) <= self._owner.position)).any() and ((np.zeros(2) < pre_position) & (pre_position < np.ones(2))).any():
+            if self._owner.position[0] <= 0 or self._owner.position[0] >= 1:
+                self.forward[0] *= -1
+            if self._owner.position[1] <= 0 or self._owner.position[1] >= 1:
+                self.forward[1] *= -1
