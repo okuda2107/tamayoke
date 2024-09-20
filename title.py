f1b51bb642b13e78cdf02fa481c7bc7bba78bfb6
okuda <okuda2107@gmail.com>
2024-09-21 02:44:22 +0900

    debug print削除

--- title.py
+++ title.py
@@ -24,8 +24,6 @@ class Title(Actor):
 
         self.button = Actor(self.game)
         self.button.position = self.game.screen_size * [0.5, 0.6]
-        print(self.button.position)
-        print(self.pointer[0].position, self.pointer[1].position)
         sc = SpriteComponent(self.button)
         sc.set_image('asset/red.png', (200, 150))
         bc = BoxComponent(self.button)
